50170fdf81becda00c7e522e3ab35a9c491d6655
Alexey Bessudnov <a.bessudnov@exeter.ac.uk>
2018-03-29 15:49:08 +0100

    modelling update

--- removeObjects.R
+++ removeObjects.R
@@ -1,3 +1,9 @@
 # This is a script to remove all the objects from R memory to start every Rmd file from fresh
 
 rm(list = ls())
+
+# I also set knitr options here
+
+knitr::opts_chunk$set(message = FALSE)
+knitr::opts_chunk$set(warning = FALSE)
+
